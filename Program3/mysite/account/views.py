378fe5aab62b34702b94af39d2270f8960a25739
Seunghyun Lee <shlee0929@icloud.com>
2020-03-05 19:54:27 +0900

    views.py refactoring

--- Program3/mysite/account/views.py
+++ Program3/mysite/account/views.py
@@ -5,43 +5,45 @@ from django.db import IntegrityError
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.utils.datastructures import MultiValueDictKeyError
-from django.views import generic
 from django.contrib.auth.models import User
 from django.contrib import auth
 
-from .models import UserAccount
+
+class PageAddress(Enum):
+    SIGN_IN = 'account/sign_in.html'
+    SIGN_UP = 'account/sign_up.html'
 
 
 class ErrorName(Enum):
-    email = 'error_email'
-    pw = 'error_pw'
-    pw_confirm = 'error_pw_confirm'
+    EMAIL = 'error_email'
+    PW = 'error_pw'
+    PW_CONFIRM = 'error_pw_confirm'
 
 
 class FormName(Enum):
-    email = 'user_email'
-    pw = 'user_pw'
-    pw_confirm = 'user_pw_confirm'
+    EMAIL = 'user_email'
+    PW = 'user_pw'
+    PW_CONFIRM = 'user_pw_confirm'
 
 
 class ErrorMessage(Enum):
-    email_empty = '메일을 입력해주세요'
-    email_invalid = '메일 형식이 잘못되었습니다'
-    email_exist = '이미 가입된 메일입니다'
-    sign_in_error = '메일 또는 비밀번호가 틀렸습니다'
-    pw_empty = '비밀번호를 입력해주세요'
-    pw_short = '비밀번호를 7자 이상 입력해주세요'
-    pw_confirm_empty = '비밀번호를 한 번 더 입력해주세요'
-    pw_confirm_wrong = '비밀번호가 일치하지 않습니다'
+    EMAIL_EMPTY = '메일을 입력해주세요'
+    EMAIL_INVALID = '메일 형식이 잘못되었습니다'
+    EMAIL_EXIST = '이미 가입된 메일입니다'
+    SIGN_IN_ERROR = '메일 또는 비밀번호가 틀렸습니다'
+    PW_EMPTY = '비밀번호를 입력해주세요'
+    PW_SHORT = '비밀번호를 7자 이상 입력해주세요'
+    PW_CONFIRM_EMPTY = '비밀번호를 한 번 더 입력해주세요'
+    PW_CONFIRM_WRONG = '비밀번호가 일치하지 않습니다'
 
 
 def index(request):
-    return render(request, 'account/sign_in.html', {})
+    return render(request, PageAddress.SIGN_IN.value)
 
 
 def sign_in(request):
-    email = request.POST[FormName.email.value]
-    pw = request.POST[FormName.pw.value]
+    email = request.POST[FormName.EMAIL.value]
+    pw = request.POST[FormName.PW.value]
 
     err_name, err_str = check_error(email, pw)
 
@@ -51,10 +53,9 @@ def sign_in(request):
     user = auth.authenticate(request, username=email, password=pw)
     if user is not None:
         auth.login(request, user)
-        return HttpResponse("sign in succeed!!<br>id: %s" % request.POST[FormName.email.value])
+        return HttpResponse("sign in succeed!!<br>id: %s" % request.POST[FormName.EMAIL.value])
     else:
-        return render_custom(request, ErrorName.email.value, ErrorMessage.sign_in_error.value)
-        # check HttpRedirect
+        return render_custom(request, ErrorName.EMAIL.value, ErrorMessage.SIGN_IN_ERROR.value)
 
 
 def find_email(request):
@@ -66,13 +67,13 @@ def find_password(request):
 
 
 def show_sign_up(request):
-    return render(request, 'account/sign_up.html', {})
+    return render(request, PageAddress.SIGN_UP.value)
 
 
 def sign_up(request):
-    email = request.POST[FormName.email.value]
-    pw = request.POST[FormName.pw.value]
-    pw_confirm = request.POST[FormName.pw_confirm.value]
+    email = request.POST[FormName.EMAIL.value]
+    pw = request.POST[FormName.PW.value]
+    pw_confirm = request.POST[FormName.PW_CONFIRM.value]
 
     err_name, err_str = check_error(email, pw, pw_confirm)
 
@@ -81,25 +82,25 @@ def sign_up(request):
 
     try:
         User.objects.create_user(email, None, pw)
-        return HttpResponse("sign up succeed!!<br>id: %s" % request.POST[FormName.email.value])
+        return HttpResponse("sign up succeed!!<br>id: %s" % request.POST[FormName.EMAIL.value])
     except IntegrityError:
-        return render_custom(request, ErrorName.email.value, ErrorMessage.email_exist.value)
+        return render_custom(request, ErrorName.EMAIL.value, ErrorMessage.EMAIL_EXIST.value)
 
 
 def check_error(email, pw, pw_confirm=None):
     if email == '':
-        return ErrorName.email.value, ErrorMessage.email_empty.value
+        return ErrorName.EMAIL.value, ErrorMessage.EMAIL_EMPTY.value
     if not re.search("[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]+", email):
-        return ErrorName.email.value, ErrorMessage.email_invalid.value
+        return ErrorName.EMAIL.value, ErrorMessage.EMAIL_INVALID.value
     if pw == '':
-        return ErrorName.pw.value, ErrorMessage.pw_empty.value
+        return ErrorName.PW.value, ErrorMessage.PW_EMPTY.value
     if pw_confirm is not None:
         if not re.search(".{7}", pw):
-            return ErrorName.pw.value, ErrorMessage.pw_short.value
+            return ErrorName.PW.value, ErrorMessage.PW_SHORT.value
         if pw_confirm == '':
-            return ErrorName.pw_confirm.value, ErrorMessage.pw_confirm_empty.value
+            return ErrorName.PW_CONFIRM.value, ErrorMessage.PW_CONFIRM_EMPTY.value
         if pw != pw_confirm:
-            return ErrorName.pw_confirm.value, ErrorMessage.pw_confirm_wrong.value
+            return ErrorName.PW_CONFIRM.value, ErrorMessage.PW_CONFIRM_WRONG.value
         else:
             return None, None
     else:
@@ -107,22 +108,23 @@ def check_error(email, pw, pw_confirm=None):
 
 
 def render_custom(request, err_name, err_str):
-    email = request.POST[FormName.email.value]
-    pw = request.POST[FormName.pw.value]
-    try:
-        pw_confirm = request.POST[FormName.pw_confirm.value]
+    email = request.POST[FormName.EMAIL.value]
+    pw = request.POST[FormName.PW.value]
 
-        return render(request, 'account/sign_up.html',
+    try:
+        pw_confirm = request.POST[FormName.PW_CONFIRM.value]
+
+        return render(request, PageAddress.SIGN_UP.value,
                       {
-                          FormName.email.value: email,
-                          FormName.pw.value: pw,
-                          FormName.pw_confirm.value: pw_confirm,
+                          FormName.EMAIL.value: email,
+                          FormName.PW.value: pw,
+                          FormName.PW_CONFIRM.value: pw_confirm,
                           err_name: err_str
                       })
     except MultiValueDictKeyError:
-        return render(request, 'account/sign_in.html',
+        return render(request, PageAddress.SIGN_IN.value,
                       {
-                          FormName.email.value: email,
-                          FormName.pw.value: pw,
+                          FormName.EMAIL.value: email,
+                          FormName.PW.value: pw,
                           err_name: err_str
                       })
